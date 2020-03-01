021ea57bbfb8b64846763269641031b082c9b133
isabelarochas <bela.rsantos@gmail.com>
2020-03-01 14:57:58 -0300

    building react app

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,27 @@ import { Route, Switch } from "react-router-dom";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
+import ResetPassword from "./containers/ResetPassword";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes({ appProps }) {
     return (
         <Switch>
             <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-            <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+            <UnauthenticatedRoute path="/login" exact component={Login} appProps={appProps} />
+            <UnauthenticatedRoute path="/signup" exact component={Signup} appProps={appProps} />
+            <AuthenticatedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+            <AuthenticatedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+            <UnauthenticatedRoute
+                path="/login/reset"
+                exact
+                component={ResetPassword}
+                props={appProps}
+            />
             { /* Finally, catch all unmatched routes */ }
             <Route component={NotFound} />
         </Switch>
